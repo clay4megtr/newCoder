6a0d58b3d36dc447b666f311dabd8008203c4d25
loushang <loushang@vipkid.com.cn>
2019-08-13 13:20:59 +0800

    字符串问题 && 随机数

---
+++ advanced/src/main/java/org/clay/Book_Re_String/KuoHao.java
@@ -0,0 +1,9 @@
+package org.clay.Book_Re_String;
+
+/**
+ * 1. 判断整体是否是有效的括号字符串
+ * 2. 给定一个括号字符串，返回最长的有效括号字符串
+ */
+public class KuoHao {
+
+}
---
+++ advanced/src/main/java/org/clay/Book_Re_String/SubArrayNoRepeat.java
@@ -0,0 +1,7 @@
+package org.clay.Book_Re_String;
+
+/**
+ * 返回一个字符串的最长无重复子串
+ */
+public class SubArrayNoRepeat {
+}
